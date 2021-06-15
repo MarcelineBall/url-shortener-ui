f36c9c880d73084d955b04dd13785998a3141b87
Marceline Ball <marchettiball@gmail.com>
2021-06-15 11:47:56 -0600

    Add test for displaying new shortened url when user clicks submit

--- cypress/integration/formInputs_spec.js
+++ cypress/integration/formInputs_spec.js
@@ -22,5 +22,17 @@ describe('formInputs', () => {
       .should('have.value', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdesignerdoginfo.files.wordpress.com%2F2012%2F10%2Fapricot-cavoodle-puppy-on-blue-blanket.jpg&f=1&nofb=1')
   })
 
-  
+  it('should display a new shortened url when the user fills out and submits the form', () => {
+    cy.fixture('mockShortUrlsAfterPost').then((mockShortUrlsAfterPost) => {
+      cy.intercept('http://localhost:3001/api/v1/urls', mockShortUrlsAfterPost)
+    })
+    cy.get('input[name="title"]')
+      .type('Cute Puppy!')
+    cy.get('input[name="long_url"]')
+      .type('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdesignerdoginfo.files.wordpress.com%2F2012%2F10%2Fapricot-cavoodle-puppy-on-blue-blanket.jpg&f=1&nofb=1')
+    cy.get('button').click()
+    cy.get('h3').contains('Cute Puppy!')
+      .get('a').contains('http://localhost:3001/useshorturl/2')
+      .get('p').contains('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdesignerdoginfo.files.wordpress.com%2F2012%2F10%2Fapricot-cavoodle-puppy-on-blue-blanket.jpg&f=1&nofb=1')
+  })
 })
