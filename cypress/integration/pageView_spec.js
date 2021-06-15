20d2586811ad5c94b810a63cfb46d51928b34093
Marceline Ball <marchettiball@gmail.com>
2021-06-15 10:55:11 -0600

    Add fixture to beforeEach to stub get request

--- cypress/integration/pageView_spec.js
+++ cypress/integration/pageView_spec.js
@@ -1,5 +1,8 @@
 describe('pageView', () => {
   beforeEach(() => {
+    cy.fixture('mockShortUrls').then((mockShortUrls) => {
+      cy.intercept('http://localhost:3001/api/v1/urls', mockShortUrls)
+    })
     cy.visit('http://localhost:3000/')
   })
 
