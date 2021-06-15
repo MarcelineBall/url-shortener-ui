7b908e480fb2eeb7ec0abba44f6544c96fee0877
Marceline Ball <marchettiball@gmail.com>
2021-06-15 09:29:54 -0600

    Refactor setState to take in array without an extra object

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,7 +16,7 @@ export class App extends Component {
 
   componentDidMount() {
     getUrls()
-      .then(data => this.setState({ urls: data }))
+      .then(data => this.setState({ urls: data.urls }))
   }
 
   render() {
