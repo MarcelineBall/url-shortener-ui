92523ea82c48afba611f09e111a7f4214dea482f
Marceline Ball <marchettiball@gmail.com>
2021-06-15 11:04:57 -0600

    Add test for displaying shortened urls

--- cypress/integration/pageView_spec.js
+++ cypress/integration/pageView_spec.js
@@ -9,4 +9,10 @@ describe('pageView', () => {
   it('should display the title of the page', () => {
     cy.get('h1').contains('URL Shortener')
   })
+
+  it('should display the current shortened urls', () => {
+    cy.get('h3').contains('Rad pic')
+      .get('a').contains('http://localhost:3001/useshorturl/1')
+      .get('p').contains('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pets4homes.co.uk%2Fimages%2Farticles%2F2659%2Flarge%2F9-great-ways-to-raise-a-happy-health-kitten-54f1c5f2a4368.jpg&f=1&nofb=1')
+  })
 })
