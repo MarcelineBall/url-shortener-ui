f9f501163a6f1c0d8a12575ba4cd14779f1e4156
Marceline Ball <marchettiball@gmail.com>
2021-06-15 10:45:24 -0600

    Add test for title display

--- cypress/integration/pageView_spec.js
+++ cypress/integration/pageView_spec.js
@@ -2,4 +2,8 @@ describe('pageView', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
   })
+
+  it('should display the title of the page', () => {
+    cy.get('h1').contains('URL Shortener')
+  })
 })
